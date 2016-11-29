235038f43e178008b4146bd516eaee3db91f9ced
zbartl <zach.dbo@gmail.com>
2016-11-28 22:31:10 -0600

    Added summernote as FOSS wysiwyg editor, with sample custom button that dumps the html content of the editor and destroys it.

--- src/SeamsCore/Features/Page/Load.js
+++ src/SeamsCore/Features/Page/Load.js
@@ -4,14 +4,42 @@ Page.Load = {
 
     init: function () {
         Page.Load.slots = $("[data-editable='slot']");
-
-        Page.Load.slots.on("click", Page.Load.setupWysiwyg(this));
-        Page.Load.setupWysiwyg();
+        Page.Load.slots.click(Page.Load.setupWysiwyg);
     },
 
-    setupWysiwyg: function (el) {
-        var quill = new Quill(el, {
-            theme: 'snow'
+    setupWysiwyg: function (e) {
+        var HelloButton = function (context) {
+            var ui = $.summernote.ui;
+
+            // create button
+            var button = ui.button({
+                contents: '<i class="fa fa-child"/> Hello',
+                tooltip: 'hello',
+                click: function () {
+                    var html = context.invoke("code");
+                    console.log(html);
+                    context.invoke("destroy");
+                }
+            });
+            return button.render();   // return button as jquery object 
+        }
+
+        $(e.currentTarget).summernote({
+            minHeight: 300,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['fontname', ['fontname']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']],
+                ['mybutton', ['hello']]
+            ],
+            buttons: {
+                hello: HelloButton
+            }
         });
     }
 
